a6887f85665b7d845d5e455f8ccc8f94482eb49c
Ced93 <91221384+Ced93@users.noreply.github.com>
2021-11-20 18:49:53 +0000

    my app

---
+++ pastena_juice/screens/Cart.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { view } from 'react-native';
+
+export default function Cart(){
+    return <view></view>
+}
---
+++ pastena_juice/screens/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { view, Text, Image } from 'react-native';
+
+export default function Login(){
+    return 
+    <view style={{flex: 1, backgroundColor: 'red'}}>
+        
+        <Text style={{ color: "black" }}>Hi,How are you ?</Text>    
+    </view>
+}
