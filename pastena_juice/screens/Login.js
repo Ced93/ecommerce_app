40213cf3885ac24e82cbdd2753a0da3a956c11cd
Ced93 <91221384+Ced93@users.noreply.github.com>
2021-11-20 19:24:07 +0000

    My app

--- pastena_juice/screens/Login.js
+++ pastena_juice/screens/Login.js
@@ -1,10 +1,70 @@
 import React from 'react';
-import { view, Text, Image } from 'react-native';
+import {View, Text, Image, TouchableOpacity} from "react-native";
+import{MaterialCommunityIcons} from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 
-export default function Login(){
-    return 
-    <view style={{flex: 1, backgroundColor: 'red'}}>
+export default function Login({navigation}){
+    return( 
         
-        <Text style={{ color: "black" }}>Hi,How are you ?</Text>    
-    </view>
+     <View style={{ flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: "white"}}>
+         <Image
+         style={{
+            width: 200,
+            height: 200,
+            borderRadius: 20,
+            marginBottom: 50,
+            marginTop:70,
+            transform:[{rotate:"295deg"}]
+        }}
+        source={require('./images/sun.first,jpeg')} 
+        />
+        
+           <Text style={{ colo:"rgba(0,0,0,0.6)", fontSize:"24"}}>Welcome to</Text> 
+           <Text style={{ Color: "black", fontSize:"30", fontWeight: " 600", fontWeight: "bol"}}>
+                Les jus pastena
+            </Text>
+        <TouchableOpacity
+           style={{ 
+               backgroundColor : "#e3e3e3",
+                padding:10,
+                flexDirection:"row",
+                 paddingHorizontal:60,
+                 borderRadius: 10,
+                marginTop:20 
+                }}
+        >
+            <AntDesign name="google" size={24} color="blue" />
+            <Text style={{ fontSize:17, marginLeft: 10}}>Login with Gmail</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+        onPress={() => {
+            navigation.navigate("Home")
+        }}
+        activeOpacity={0.8}
+           style={{ 
+               backgroundColor:"black",
+               padding:10,
+               flexDirection:"row",
+               paddingHorizontal:60,
+               borderRadius : 10,
+               marginTop : 20,
+
+           }}
+        >
+            <AntDesign name="apple1" size={24} color="white"  />
+           <Text style={{ fontSize:17, color:"white", marginLeft: 10, paddingTop:3 }}>Login with Apple</Text>
+
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+        <Text style={{ fontSize:10, color:"grey",  paddingTop:10, fontWeight:"500" }}>Not a member? {" "}
+             <Text style={{ color : "orange", fontWeight:"bold"}}>SinUp</Text>
+        </Text> 
+        </TouchableOpacity>
+        
+        
+        </View>
+     
+    )
 }
